343180abe71d3d3270db73ea6fb72cebbcb31ede
MateusP21 <mateuspasso@outlook.com>
2022-06-30 22:47:27 -0300

    feat:  função para verificar se existe no bd

--- models/productModel.js
+++ models/productModel.js
@@ -2,15 +2,23 @@ const db = require('./db');
 
 const productModel = {
   async getAllProducts() {
-    const listAllProducts = 'SELECT * FROM StoreManager.products';
-    const list = await db.query(listAllProducts);
+    const query = `
+    SELECT *
+    FROM StoreManager.products`;
+    const list = await db.query(query);
     return list;
   },
 
   async getProductById(id) {
-    const productQuery = 'SELECT * FROM StorageManager.products WHERE id = ?';
-    const product = await db.query(productQuery, [id]);
+    const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+    const product = await db.query(query, [id]);
     return product;
+  },
+
+  async exists(id) {
+    const query = 'SELECT 1 FROM StoreManager.products WHERE id = ?';
+    const item = await db.query(query, [id]);
+    return item;
   },
 };
 

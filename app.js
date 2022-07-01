c0707e5843027e64489a285269d46d7adfb99b18
MateusP21 <mateuspasso@outlook.com>
2022-06-30 22:49:27 -0300

    feat: import express-async-erros e middleware de errors

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+require('express-async-errors');
+const errorHandler = require('./middlewares/errorMiddlewares');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 app.use(express.json());
+
 app.use('/products', productRoutes);
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use(errorHandler);
+// app.use(errorHandler);
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação

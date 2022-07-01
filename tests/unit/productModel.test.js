84ce8a28bba6d362463b47277cd376902b19a00d
MateusP21 <mateuspasso@outlook.com>
2022-07-01 18:45:38 -0300

    test: criando mock e alterando testes

--- tests/unit/productModel.test.js
+++ tests/unit/productModel.test.js
@@ -6,11 +6,20 @@ const chai = require('chai');
 
 chai.use(chaiAsPromised);
 
+const mockProducts = [
+  {
+    id: 1,
+    name: 'any_name',
+  },
+  { id: 2, name: 'any_name' },
+  { id: 3, name: 'any_name' },
+];
+
 describe('productModel', () => {
   beforeEach(sinon.restore);
   describe('getAllProducts', () => {
     it('quando chamado espera que se retorne uma lista', () => {
-      sinon.stub(db, 'query').resolves([]);
+      sinon.stub(db, 'query').resolves(mockProducts);
       return chai
         .expect(productModel.getAllProducts())
         .to.eventually.be.a('array');
@@ -25,16 +34,24 @@ describe('productModel', () => {
 
   describe('getProductById', () => {
     it('quando chamado espera que se retorne um objeto', () => {
-      sinon.stub(db, 'query').resolves({});
+      sinon.stub(db, 'query').resolves(mockProducts[0]);
       return chai
-        .expect(productModel.getProductById())
+        .expect(productModel.getProductById(1))
         .to.eventually.be.a('object');
     });
 
-    it('dispare um erro se o banco disparar um erro', () => {
-      sinon.stub(db, 'query').rejects();
-      return chai.expect(productModel.getProductById()).to.eventually.be
-        .rejected;
+    it('espera se que o objeto possua um id', () => {
+      sinon.stub(db, 'query').resolves(mockProducts[0]);
+      return chai
+        .expect(productModel.getProductById(1))
+        .to.eventually.have.property('id');
+    });
+  });
+
+  describe('exists', () => {
+    it('quando chamado espera que se retorne um numero', () => {
+      sinon.stub(db, 'query').resolves(1);
+      return chai.expect(productModel.exists(0)).to.eventually.be.a('number');
     });
   });
 });

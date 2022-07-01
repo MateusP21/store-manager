fbef02140a2a40515e4951ef9b52376fe7032ee9
MateusP21 <mateuspasso@outlook.com>
2022-06-30 22:48:24 -0300

    feat: nova rota para buscar pelo o id

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,7 @@ const productController = require('../controllers/productController');
 
 const productRoutes = Router();
 
-productRoutes.get('/', productController.get);
+productRoutes.get('/', productController.getAll);
+productRoutes.get('/:id', productController.getById);
 
 module.exports = productRoutes;
